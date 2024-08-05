089f11c7490699be62623dc9141613a56d064967
Thibault Paillon <thibault_paillon@live.fr>
2024-08-05 14:28:26 +0200

    Trying to resolve the path issues at deployment stage

--- eucalyptus_services/wsgi.py
+++ eucalyptus_services/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the project root directory to the Python path
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_root)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eucalyptus_services.settings')
 
